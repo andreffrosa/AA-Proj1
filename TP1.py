002d97d039b7ab823f98d9343ca8f29c1f39fb47
André Rosa <af.rosa@campus.fct.unl.pt>
2019-10-19 14:45:29 +0100

    Naiive Bayes with K-FoldValidation

--- TP1.py
+++ TP1.py
@@ -7,7 +7,8 @@ Created on Wed Oct 16 19:10:28 2019
 """
 
 import numpy as np
-import naiive_bayes
+from sklearn.model_selection import StratifiedKFold 
+import naiive_bayes as NB
 
 #Standardize the data
 def standardize(data, means=None, stdevs=None): # ignores the last column (classification)
@@ -27,18 +28,69 @@ train_data,means,stdevs = standardize(train_data);
 #Shuffle train_data
 np.random.shuffle(train_data)
 
+#
+    
+def calc_fold(bandwidth, X, Y, train_ix, test_ix):
+    """return classification error for train and test sets""" 
+    nb = NB.naiive_bayes_classifier(bandwidth=bandwidth);
+    nb.fit(X[train_ix,:], Y[train_ix])
+    classifications = nb.classify(X)#[:,1] 
+    
+    misclassified_train = sum(classifications[train_ix] != Y[train_ix])
+    misclassified_test = sum(classifications[test_ix] != Y[test_ix])
+    
+    error_perc_train = (float(misclassified_train)/(len(train_ix)))*100
+    error_perc_test = (float(misclassified_test)/(len(test_ix)))*100
+    
+    return (error_perc_train, error_perc_test)
+
+folds = 10 
+kf = StratifiedKFold(n_splits=folds)
+
+
+best_va_err = 101.0
+best_tr_err = 0
+best_bandwidth = 0
+
+for i in range(0,30):
+    bandwidth = (i+1)*0.02
+    tr_err = va_err = 0
+    
+    for tr_ix,va_ix in kf.split(train_data[:,:-1],train_data[:,-1]):
+        r,v = calc_fold(bandwidth,train_data[:,:-1],train_data[:,-1],tr_ix,va_ix)
+        tr_err += r 
+        va_err += v 
+        
+    tr_err = tr_err/folds
+    va_err = va_err/folds
+    print(bandwidth,':', tr_err, '\t', va_err)
+    
+    if(va_err < best_va_err):
+        best_va_err = va_err
+        best_tr_err = tr_err
+        best_bandwidth = bandwidth
+
+print('\n')
+print(best_bandwidth,':', best_tr_err, '\t', best_va_err)      
+
 #Create and train a naiive bayes classifier
-nb = naiive_bayes.naiive_bayes_classifier(bandwidth=1.0);
-nb.fit(train_data)
+nb = NB.naiive_bayes_classifier(bandwidth=best_bandwidth);
+nb.fit(train_data[:,:-1], train_data[:,-1])
+
+clasification = nb.classify(train_data[:,:-1])
+
+misclassified = sum(train_data[:,-1] != clasification)
+train_err = (float(misclassified)/(train_data.shape[0]))*100
+print(train_err)
 
 # Load the test_data matrix and standardize it
 test_data = np.loadtxt("TP1_test.tsv", delimiter='\t')
 test_data,_,_ = standardize(test_data, means=means, stdevs=stdevs)
 
-y_predicted = nb.predict(test_data)
+clasification = nb.classify(test_data[:,:-1])
 
-misclassified = sum(test_data[:,-1] != y_predicted)
-
-print(misclassified)
+misclassified = sum(test_data[:,-1] != clasification)
+test_err = (float(misclassified)/(test_data.shape[0]))*100
+print(test_err)
 
 
--- naiive_bayes.py
+++ naiive_bayes.py
@@ -11,7 +11,7 @@ import math
 
 class naiive_bayes_classifier:
     
-    def __init__(self, bandwidth):
+    def __init__(self, bandwidth=1.0):
         self.bandwidth = bandwidth
         self.P_c0 = 0
         self.P_c1 = 0
@@ -19,17 +19,21 @@ class naiive_bayes_classifier:
         self.kdes_c1 = []
         self.n_features = 0
         
-    def fit(self, train_data):
+    def fit(self, X, Y):
         """
         """
-        C0 = train_data[train_data[:,-1] == 0.0]
-        C1 = train_data[train_data[:,-1] == 1.0]
+        # Separate the training set into disjoint sets of each class
+        C0 = X[Y == 0.0]
+        C1 = X[Y == 1.0]
 
-        self.P_c0 = math.log(C0.shape[0] / train_data.shape[0])
-        self.P_c1 = math.log(C1.shape[0] / train_data.shape[0])
+        # Compute the log of the prior probability of belonging to (proportion of) each class
+        self.P_c0 = math.log(C0.shape[0] / X.shape[0])
+        self.P_c1 = math.log(C1.shape[0] / X.shape[0]) # = 1 - P-c0
         
-        self.n_features = train_data.shape[1] - 1
+        # Compute the number of features
+        self.n_features = X.shape[1]
         
+        # For each feature of each class, compute the kernel density estimation to be used as the probability distribution of each feature (of each class)
         for feature in range(self.n_features):
             kde = sk.KernelDensity(bandwidth=self.bandwidth, algorithm='auto', kernel='gaussian', metric='euclidean')
             kde.fit(C0[:,feature].reshape(-1, 1))
@@ -39,13 +43,19 @@ class naiive_bayes_classifier:
             kde.fit(C1[:,feature].reshape(-1, 1))
             self.kdes_c1.append(kde)
             
-    def predict(self, examples):
+    def classify(self, X):
         """
         """
+        # Start with the prior probability
         pc0 = self.P_c0
         pc1 = self.P_c1
+        
+        # For each feature, add the log of the conditional probability of the feature given the class
         for feature in range(self.n_features):
-            pc0 += self.kdes_c0[feature].score_samples(examples[:,feature].reshape((-1,1)))
-            pc1 += self.kdes_c1[feature].score_samples(examples[:,feature].reshape((-1,1)))
+            pc0 += self.kdes_c0[feature].score_samples(X[:,feature].reshape((-1,1)))
+            pc1 += self.kdes_c1[feature].score_samples(X[:,feature].reshape((-1,1)))
     
+        # Choose the highest probability as the class of each example
         return pc1 > pc0
+    
+    
